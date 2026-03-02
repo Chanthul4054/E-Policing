821731a1d7713d82bc0b96e901eb3bc99beed8ae
Wishmiii <subasinghewishmi@gmail.com>
2026-03-02 10:06:10 +0530

    Update risk factor pipeline to use DB derived features, contexual averages and updated feature list

--- services/risk_service.py
+++ services/risk_service.py
@@ -6,7 +6,7 @@ import os
 import shap
 from extensions import db
 from sqlalchemy import text
-
+from datetime import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODEL_DIR = os.path.join(BASE_DIR,"models")
@@ -34,7 +34,7 @@ def run_risk_factor_pipeline(crime_type, features):
     if crime_type not in CRIME_MODELS:
         raise ValueError("Invalid crime type")
 
-    # convert to dataframe - (ml models requires tabular input, feature order must match training)
+    # convert to dataframe 
     X = pd.DataFrame([features])
     X = X[FEATURE_ORDER]
 
@@ -50,9 +50,19 @@ def run_risk_factor_pipeline(crime_type, features):
 
     # SHAP
     explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shape_values(X_scaled)
+    shap_values = explainer.shap_values(X_scaled)
 
-    shap_values_pos = shap_values[:,:,1]
+    # Handle different SHAP output shapes
+    if isinstance(shap_values, list):
+        shap_values_pos = shap_values[1]
+    elif len(shap_values.shape) ==3:
+        shap_values_pos = shap_values[:,:,1]
+    else:
+        shap_values_pos = shap_values
+
+    shap_row = shap_values_pos[0]
+
+
 
     # get top 3 drivers
     shap_row = shap_values_pos[0]
@@ -63,34 +73,15 @@ def run_risk_factor_pipeline(crime_type, features):
 
     top_drivers = (
         feature_importance
-        .reindex(feature_importance.shap_values.abs().sort_values(ascending=False).index)
+        .reindex(feature_importance["shap_value"].abs().sort_values(ascending=False).index)
         .head(3)
     )
 
     return {
         "risk_score":round(risk, 3),
         "top_features":top_drivers["feature"].tolist(),
-        "shap_values":top_drivers["shap_values"].tolist()
+        "shap_values":top_drivers["shap_value"].tolist()
     }
-
-    # SHAP
-
-    # explainer = shap.TreeExplainer(rf_model)
-
-    # shap_values = explainer.shap_values(X_test_scaled)
-
-    # shap_values_pos = shap_values[:,:,1]
-    # base_value = explainer.expected_value[1]
-
-    # global_summary = get_global_summary_table(shap_values_pos, X_test_scaled.columns)
-
-    # dirrectional_effect = get_global_directional_effect_table(shap_values_pos, X_test_scaled)
-
-
-    # local_results = []
-
-    # for i in range(len(X_test_scaled)):
-    #     top_drivers = get_local_top_drivers(shap_values_pos[i],X_test_scaled.iloc[i])
 
 
 # fetch the GN list 
@@ -98,69 +89,97 @@ def get_all_gns():
     query = text("""
         SELECT DISTINCT gn_division
         FROM crime_data
-        ORDER BY gn_pcode    
+        ORDER BY gn_division    
     """)
 
     with db.engine.connect() as conn:
-        rows = conn.execute(query).fetchall()
+        gn_name_list = conn.execute(query).fetchall()
 
-    return [row[0] for row in rows]
+    return [row[0] for row in gn_name_list]
 
 
 
 
 # fetch gn level environmental, socio-economic, demographic,... data
-def fetch_gn_features(gn_division):
-    query = text("""
+def fetch_gn_features(gn_division_name):
+    # 1)Fetch structural features
+    query_struct = text("""
         SELECT
-            GN_population,
-            AVG_Household_income,
-            Unemployement_Rate,
-            Builiding Density,
-            Road_Density,
-            distance_to_station_km
-        FROM crime_data,
-        WHERE gn_division = :gn,
-        ORDER BY year DESC
+            "GN_population",
+            "Avg_Household_Income",
+            "Unemployment_Rate",
+            "Building_Density",
+            "Road_Density",
+            "distance_to_station_km"
+        FROM gn_division_info
+        WHERE "admin4Name_en" = :gn
         LIMIT 1
     """)
 
-    with db.engine.connect() as conn():
-        row = conn.execute(query, {"gn":gn_division}),mappings().first()
+    with db.engine.connect() as conn:
+        struct = conn.execute(query_struct, {"gn":gn_division_name}).mappings().first()
 
-    if not row:
-        raise ValueError("GN not found")
+    if not struct:
+        raise ValueError("GN Division not found")
     
-    return dict(row)
+    # 2)Fetch contexual averages 
+    query_contexual = text("""
+        SELECT
+            AVG(victim_age) AS avg_victim_age,
+            AVG(CASE WHEN sex = 'f' THEN 1 ELSE 0 END) AS female_ratio,
+            AVG(CASE WHEN is_holiday = '1' THEN 1 ELSE 0 END) AS holiday_ratio,
+            AVG(CASE WHEN LOWER(weather) = 'rainy' THEN 1 ELSE 0 END) AS rainy_ratio
+        FROM crime_data
+        WHERE gn_division = :gn
+    """
+    )
+    with db.engine.connect() as conn:
+        context = conn.execute(query_contexual, {"gn":gn_division_name}).mappings().first()
+
+    
+    # if not context:
+    #     raise ValueError("GN Division not found")
+
+    # Handling missing values safely
+    avg_victim_age = context["avg_victim_age"] or 30
+    # female_victim_ratio = context["female_ratio"] or 0
+    holiday_ratio = context["holiday_ratio"] or 0
+    rainy_ratio = context["rainy_ratio"] or 0
+    
+    # 3) Urban ratios
+    # temporary placeholders ################
+    land_area_density = 0.5
 
 
+    # 4) Transform to model schema
+    gn_population = struct["GN_population"] or 1
+    log_population = np.log1p(gn_population)
+
+    distance_km = struct["distance_to_station_km"] or 0
+
+    # distance_km = struct(static placeholder for now)
+    current_week = datetime.now().isocalendar().week
+    week_sin = np.sin(2 * np.pi * current_week / 52)
+    week_cos = np.cos(2 * np.pi * current_week / 52)
+
+    year = datetime.now().year
 
 
+    features = {
+        "avg_victim_age": float(avg_victim_age),
+        "holiday_ratio": float(holiday_ratio),
+        "rainy_ratio": float(rainy_ratio),
+        "unemployment_rate": float(struct["Unemployment_Rate"] or 0),
+        "avg_income": float(struct["Avg_Household_Income"] or 0),
+        "building_density": float(struct["Building_Density"] or 0),
+        "land_area_density": float(land_area_density),
+        "road_density": float(struct["Road_Density"] or 0),
+        "log_population": float(log_population),
+        "distance_km": float(distance_km),
+        "year": float(year)
+    }
 
-# gn_division :  ['Welata' 'Katukele West' 'Penideniya' 'Aniwatta East' 'Aruppala East'
-#  'Suduhumpala West' 'Mahaiyawa' 'Boowelikada' 'Asgiriya' 'Thennakumbura'
-#  'Bowala' 'Mahanuwara' 'Thalwatta' 'Ampitiya Udagama North'
-#  'Thalathuoya East' 'Poorna Watta West' 'Bogambara' 'Ihala Katukele'
-#  'Suduhumpala East' 'Bowalawatta' 'Poorna Watta East' 'Ihala Dodamwala'
-#  'Sirimalwatta West' 'Sirimalwatta East' 'Ampitiya Pallegama' 'Batagalla'
-#  'Uda Bowala' 'Kotagepitiya' 'Watapuluwa West' 'Hanthana Pedesa'
-#  'Ampitiya North' 'Nagasthenna' 'Bahirawa Kanda' 'Getambe' 'Nittawela'
-#  'Pallegunnepana North' 'Lewella' 'Mahaweli Uyana' 'Hippola'
-#  'Pitakanda Gama' 'Deiyannewela' 'Ampitiya South' 'Kendakaduwa'
-#  'Watapuluwa South' 'Kumburegedara' 'Kundasale North' 'Watapuluwa'
-#  'Pallegunnepana South' 'Dehiwatta' 'Palle Peradeniya' 'Katukele'
-#  'Ogastawatta' 'Aniwatta West' 'Meddegama' 'Inguruwatta' 'Wattegama North'
-#  'Kalugalawatta' 'Malwatta' 'Pitakanda' 'Mulgampala'
-#  'Gurudeniya Dambawela' 'Wewathenna' 'Godagandeniya' 'Haloluwa'
-#  'Uduwelawatta Colony' 'Uda Eriyagama East' 'Arangala South'
-#  'Wathurakumbura' 'Mapanawathura' 'Thalathuoya West' 'Ulpathakumbura']
+    return features
 
 
-            # land_area_density,
-            # road_density,
-            # log_population,
-            # distance_km,
-            # year,
-            # week_sin,
-            # week_cos,
         
