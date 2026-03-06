8256f50aca2f5f5867fb699d0631682e9a1a36fa
Haniffa <yumna.2005@iit.ac.lk>
2026-03-07 01:17:08 +0530

    made changes to the file paths

--- routes/hotspot_routes.py
+++ routes/hotspot_routes.py
@@ -14,25 +14,25 @@ hotspot_bp = Blueprint("hotspot", __name__)
 
 #Path Setup
 current_dir = os.path.dirname(os.path.abspath(__file__)) 
-base_path = os.path.abspath(os.path.join(current_dir, "..", "..", ".."))
+base_path = os.path.abspath(os.path.join(current_dir, ".."))
 
 
 
 # Load global assets
-scaler = joblib.load(os.path.join(base_path, 'feature_scaler.joblib'))
-with open(os.path.join(base_path, 'gn_name_mapping.json'), 'r') as f:
+scaler = joblib.load(os.path.join(base_path,'models','feature_scaler.joblib'))
+with open(os.path.join(base_path, 'static', 'gn_div_info','gn_name_mapping.json'), 'r') as f:
     gn_map = {int(k): v for k, v in json.load(f).items()}
 
 #The logic
 def generate_risk_scores(crime_type):
     
     #Load Master feature list
-    with open(os.path.join(base_path, 'feature_list.json'), 'r') as f:
+    with open(os.path.join(base_path, 'models','feature_h_list.json'), 'r') as f:
         master_features = json.load(f)
     scaler_features = [f for f in master_features if f != 'gn_encoded']
 
     #Data Prep
-    inference_df = pd.read_parquet(os.path.join(base_path, 'inference_data_latest.parquet'))
+    inference_df = pd.read_parquet(os.path.join(base_path,'models', 'inference_data_latest.parquet'))
     clean_df = inference_df.drop_duplicates(subset=['gn_encoded'], keep='last').copy()
     
     #Scaling
@@ -41,8 +41,11 @@ def generate_risk_scores(crime_type):
     scaled_df['gn_encoded'] = clean_df['gn_encoded'].values
     
     #Model setup
-    model = joblib.load(os.path.join(base_path, 'models', f'model_{crime_type}.pkl'))
-    model_features = joblib.load(os.path.join(base_path, 'model features', f'{crime_type}_features.pkl'))
+    model_path = os.path.join(base_path, 'models', f'model_{crime_type}.pkl')
+    model = joblib.load(model_path)
+
+    feature_file = os.path.join(base_path, 'models', f'{crime_type}_features.pkl')
+    model_features = joblib.load(feature_file)
     
     #Predict
     risk_scores = model.predict_proba(scaled_df[model_features])[:, 1]
